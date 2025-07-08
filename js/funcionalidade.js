52e39e35dd3236161cb264a43a7d554a5e9040f5
Lucas Sant <lucasfdesantana095@gmail.com>
2025-07-08 11:11:28 -0300

    criacao de condicoes para o tratanto erros

--- js/funcionalidade.js
+++ js/funcionalidade.js
@@ -1,46 +1,78 @@
 listaUsuarios = [] // lista para armazenar usuarios
 
-// const nomeInformado = document.querySelector("#nome");
-// const idadeInformada = document.querySelector("#idade");
-// const materialInformado = document.querySelector("#numero-material");
-// const pesoMaterialInformado = document.querySelector("#peso-material");
 
 // Funcionalidades paracadastrarusuários--------------------------------------------------------------------------
 const finalizandoSessao = document.getElementById("enviar-valores");
 
 finalizandoSessao.addEventListener('click', () =>{
-    let nomeInformado = document.querySelector("#nome");
-    let idadeInformada = document.querySelector("#idade");
-    let materialInformado = document.querySelector("#numero-material");
-    let pesoMaterialInformado = document.querySelector("#peso-material");
+    let nomeInformado = document.querySelector("#nome").value;
+    let idadeInformada = document.querySelector("#idade").value;
+    let materialInformado = document.querySelector("#numero-material").value;
+    let pesoMaterialInformado = document.querySelector("#peso-material").value;
 
-    let inputNome = nomeInformado.value;
-    let inputIdade = idadeInformada.value;
-    let inputMaterial = materialInformado.value;
-    let inputPeso = pesoMaterialInformado.value;
+    // lidando com tratamento de erros
 
-    informacaoUsuario = [inputNome, inputIdade, inputMaterial, inputPeso]
-    // alert(info)
+    // se a idade informada for menor que 16 anos
+    if(idadeInformada <= 16){
+        document.querySelector("#nome").value = "";
+        document.querySelector("#idade").value = "";
+        document.querySelector("#numero-material").value = "";
+        document.querySelector("#peso-material").value = "";
+    }
+
+    // se o nº do máterial não estiver entre 1, 2, 3, 4 e 5
+    else if (materialInformado != 1 || 2 || 3 || 4 || 5){
+        alert('Por favor, informe o material corretamente!');
+        document.querySelector("#nome").value = "";
+        document.querySelector("#idade").value = "";
+        document.querySelector("#numero-material").value = "";
+        document.querySelector("#peso-material").value = "";
+    }
+
+    // se tudo estiver correto
+    else{
+        const usuarioCadastrado = { nomeInformado, idadeInformada, materialInformado, pesoMaterialInformado }
+        listaUsuarios.push(usuarioCadastrado);
+        alert(`O cadastro de ${nomeInformado} foi concluído com sucesso!`);
+
+        // deixando os campos vazios novamente
+        document.querySelector("#nome").value = "";
+        document.querySelector("#idade").value = "";
+        document.querySelector("#numero-material").value = "";
+        document.querySelector("#peso-material").value = "";
+    }
+
 })
+
 
 // Funcionalidades para o botão de usuários --------------------------------------------------------------------------
 const botaoUsuario = document.getElementById('botao-usuarios');
 const botaoRetorno = document.getElementById('botao-retornar');
-const imagemInicial = document.getElementById('imagem-inicio')
+const imagemInicial = document.getElementById('imagem-inicio');
+const mensagemInicial = document.getElementById('mensagem-inicial');
 
 const inputsHTML = document.getElementsByClassName("input-botao");
 const centralizarCheckboxHTML = document.getElementById("centralizar-checkbox");
 const caixaCheckboxHTML = document.getElementById('campo-checkbox')
 
 // função para fazer o botão de usuários desaparecer e o botão de retorno aparecer
-botaoUsuario.addEventListener('click', MouseEvent => {
+botaoUsuario.addEventListener('click', () => {
     for (let index = 0; index < inputsHTML.length; index++) {
     inputsHTML[index].style.display = "none" ;
     }
     imagemInicial.style.display = "none";
     caixaCheckboxHTML.style.display = "none";
-    botaoRetorno.style.display = 'flex'
-    botaoUsuario.style.display = 'none'
+    botaoRetorno.style.display = 'flex';
+    botaoUsuario.style.display = 'none';
+    mensagemInicial.style.display = 'none';
+
+    function mostrarUsuarios(){
+        let mensagem = 'Não há nenhum usuário cadastrado!';
+        if(listaUsuarios.length === 0){
+            document.getElementById("mostrar-usuarios").innerHTML = `${mensagem}`
+
+        }
+    }
    
 })
 
@@ -48,13 +80,32 @@ botaoUsuario.addEventListener('click', MouseEvent => {
 // os inputs e imagens
 botaoRetorno.style.display = "none";
 
-botaoRetorno.addEventListener('click', MouseEvent => {
+botaoRetorno.addEventListener('click', () => {
     for (let index = 0; index < inputsHTML.length; index++) {
     inputsHTML[index].style.display = "flex" ;
     }
     imagemInicial.style.display = "flex";
     caixaCheckboxHTML.style.display = "flex";
-    botaoUsuario.style.display = 'flex'
-    botaoRetorno.style.display = 'none'
+    botaoUsuario.style.display = 'flex';
+    botaoRetorno.style.display = 'none';
+    mensagemInicial.style.display = 'flex';
 
 })
+
+
+
+
+
+// Criando class para gerar pontos do usuário
+// class pontosUsuario{
+//     constructor(peso, valorMercado){
+//         this.peso = peso;
+//         this.valorMercado = valorMercado;
+//     }
+
+//     pontosGerados(){
+//         peso * valorMercado
+//     }
+// }
+
+// const 
